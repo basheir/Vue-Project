3f7589c8b2549adee9dfda12ab80df62334da64d
Bashir Mahad <basheirmahad@gmail.com>
2023-06-14 06:36:53 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- Header Navbar -->
+    <Navbar></Navbar>
+    <!-- Route view -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/partials/Navbar.vue'
+export default {
+
+  components: {
+    Navbar
+  },
+  mounted() {
+    console.warn('App root component mounted')
+  }
+}
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-danger">Home component</h1>
+</template>
